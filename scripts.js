2397622aec1329e6b0470f89cead3259db0da4bd
guilherme gouveia da costa <guigc0408@gmail.com>
2023-01-19 00:57:21 -0300

    versão teste layout base mobile

---
+++ scripts.js
@@ -0,0 +1,22 @@
+var nomeOnline;
+function login() {
+    const nomeLogin = { name: prompt("Informe o nome de usuário.") };
+    console.log(nomeLogin);
+    const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeLogin)
+    requisicao.then(tudoCerto);
+    requisicao.catch(algoErrado);
+    function tudoCerto(resposta) {
+        setInterval(confirmaOnline, 5000, nomeLogin);
+    }
+    function algoErrado(resposta) {
+        console.log(resposta);
+    }
+}
+function confirmaOnline(nome) {
+    nomeOnline = nome;
+    const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nomeOnline);
+    requisicao.then(tudoCerto);
+    function tudoCerto(resposta){
+        //online
+    }
+}
