828df7e6f89e3c42d9afceffb84ee87044c37a6d
guilherme gouveia da costa <guigc0408@gmail.com>
2023-01-21 14:37:27 -0300

    versão com layout mensagens pronto

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 var nomeOnline;
+recebeMensagens();
 function login() {
     const nomeLogin = { name: prompt("Informe o nome de usuário.") };
     console.log(nomeLogin);
@@ -16,7 +17,14 @@ function confirmaOnline(nome) {
     nomeOnline = nome;
     const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nomeOnline);
     requisicao.then(tudoCerto);
-    function tudoCerto(resposta){
+    function tudoCerto(resposta) {
         //online
+    }
+}
+function recebeMensagens() {
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then(mostraMensagem)
+    function mostraMensagem(mensagens) {
+        console.log(mensagens);
     }
 }
